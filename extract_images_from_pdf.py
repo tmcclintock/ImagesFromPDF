94157bf160ccba03bda13c1c4793a47153902c08
Tom McClintock <tmcclintock89@gmail.com>
2020-07-07 20:54:46 -0700

    Set default flags correctly.

--- extract_images_from_pdf.py
+++ extract_images_from_pdf.py
@@ -5,25 +5,53 @@ Script for extracting PDF images to an album.
 import argparse, glob, os, sys
 from pdfreader import PDFDocument
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def main():
     # Parse the arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("file_name")
-    parser.add_argument("-o", "--output", default=None)
-    parser.add_argument("-v", "--verbose", default=True,
+    parser.add_argument("-o", "--output", default=None,
+                        help="sets the output directory")
+    parser.add_argument("-v", "--verbose", default=False, type=str2bool,
+                        const=True, nargs='?',
                         help="increase output verbosity")
     parser.add_argument("-fp", "--first_page", default=0,
                         help="first page to extract from")
     parser.add_argument("-lp", "--last_page", default=1000,
                         help="last page to extract from")
-    parser.add_argument("-mw", "--min_width", default=200)
-    parser.add_argument("-mh", "--min_height", default=200)
-    parser.add_argument("-xw", "--max_width", default=1210)
-    parser.add_argument("-xh", "--max_height", default=1570)
-    parser.add_argument("-mt", "--make_transparent", default=True)
-    parser.add_argument("-wt", "--white_to_trans", default=True)
-    parser.add_argument("-bt", "--black_to_trans", default=True)
+    parser.add_argument("-mw", "--min_width", default=200,
+                        help="minimum pixel width")
+    parser.add_argument("-mh", "--min_height", default=200,
+                        help="minimum pixel height")
+    parser.add_argument("-xw", "--max_width", default=1210,
+                        help="maximum pixel width")
+    parser.add_argument("-xh", "--max_height", default=1570,
+                        help="maximum pixel height")
+    parser.add_argument("-mt", "--make_transparent", default=True,
+                        type=str2bool, const=False, nargs='?',
+                        help="flag to make the background transparent")
+    parser.add_argument("-wt", "--white_to_trans", default=True,
+                        type=str2bool, const=False, nargs='?',
+                        help="turn white pixels transparent")
+    parser.add_argument("-bt", "--black_to_trans", default=True,
+                        type=str2bool, const=False, nargs='?',
+                        help="turn black pixels transparent")
+    parser.add_argument("-wf", "--white_fuzz", default=1,
+                        help="fuzz percent (0-100) for white transparency")
+    parser.add_argument("-bf", "--black_fuzz", default=1,
+                        help="fuzz percent (0-100) for black transparency")
+    parser.add_argument("-ims", "--image_string", default="Im",
+                        help="string that appears in all image names")
     args = parser.parse_args()
 
     if args.verbose:
@@ -69,7 +97,7 @@ def main():
 
         # Loop over possible image objects
         for key in page.Resources.XObject.keys():
-            if "Im" in key or "im" in key:
+            if args.image_string in key or "im" in key:
                 xobj = page.Resources.XObject[key]
                 try:
                     pil_image = xobj.to_Pillow()
@@ -90,6 +118,7 @@ def main():
                         pil_image.save(f"{output}/page{i}_{key}.png")
                         if args.make_transparent:
                             _do_transparent(args, i, key, pil_image, output)
+                            print("did this")
     return
 
 def _do_transparent(args, i, key, im, output):
@@ -114,20 +143,29 @@ def _do_transparent(args, i, key, im, output):
 
     def check_result(res):
         if res != 0:
-            raise Exception("'convert' from ImageMagick not recognized")
+            raise Exception(
+                "'convert' from ImageMagick not recognized " +
+                "either install ImageMagick or run with " +
+                "--make_transparent=False"
+            )
         pass
 
     # Make the command and execute
     if args.white_to_trans:
-        cmd = f"convert {inpath} -fuzz 1% -transparent white {outpath}"
+        assert 0 <= args.white_fuzz <= 100, "white_fuzz must be within 0-100"
+        cmd = f"convert {inpath} -fuzz {args.white_fuzz}% -transparent " + \
+            f"white {outpath}"
         res = os.system(cmd)
         check_result(res)
         if args.black_to_trans:
             inpath = outpath
     if args.black_to_trans:
-        cmd = f"convert {inpath} -fuzz 1% -transparent black {outpath}"
+        assert 0 <= args.black_fuzz <= 100, "black_fuzz must be within 0-100"
+        cmd = f"convert {inpath} -fuzz {args.black_fuzz}% -transparent " + \
+            f"black {outpath}"
         res = os.system(cmd)
         check_result(res)
+        print("here  ",outpath)
     return 0
 
 
